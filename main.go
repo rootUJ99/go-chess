900425a0304142c1767ddfccc042eb98d7cd2c7e
ujwal arak <ujwal99arak@gmail.com>
2023-10-22 23:59:44 +0530

    starting to render text still needs some improvements here and there

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	tl "github.com/JoelOtter/termloop"
 )
@@ -25,6 +27,23 @@ type Game struct {
 	drawnBoard *board
 	game *tl.Game
 	level *tl.BaseLevel
+	gotya Gotya
+}
+
+type GotyaWB struct {
+	Hatti1 string `json:"hatti1"`
+	Ghoda1 string `json:"ghoda1"`
+	Unta1 string `json:"unta1"`
+	Vajeer string `json:"vajeer"`
+	Raja string `json:"raja"`
+	Hatti2 string `json:"hatti2"`
+	Ghoda2 string `json:"ghoda2"`
+	Unta2 string `json:"unta2"`
+}
+
+type Gotya struct {
+	White GotyaWB `json:"white"` 
+	Black GotyaWB `json:"black"` 
 }
 
 func constructBoard() board{
@@ -47,9 +66,42 @@ func (g Game) paintBoard() {
 	for rowIndex, row := range g.drawnBoard {
 		y:= rowIndex * 6
 		for colIndex, col:= range row {
-		 x:= colIndex * 12
+			x:= colIndex * 12
 			g.level.AddEntity(tl.NewRectangle(x, y, 12, 6, col.color))
-		} 
+			var gotiColor tl.Attr
+			if col.color == tl.ColorBlack {
+				gotiColor = tl.ColorWhite
+			} else {
+				gotiColor = tl.ColorBlack
+			}
+			switch rowIndex {
+			case 0:
+				fmt.Println("im here inside", colIndex, g.gotya.White.Hatti2)
+				switch colIndex {
+				case 0: g.level.AddEntity(tl.NewText(x,y, g.gotya.White.Hatti1, gotiColor, col.color))
+				case 1: g.level.AddEntity(tl.NewText(x,y, g.gotya.White.Ghoda1, gotiColor, col.color))
+				case 2: g.level.AddEntity(tl.NewText(x,y, g.gotya.White.Unta1, gotiColor, col.color))
+				case 3: g.level.AddEntity(tl.NewText(x,y, g.gotya.White.Vajeer, gotiColor, col.color))
+				case 4: g.level.AddEntity(tl.NewText(x,y, g.gotya.White.Raja, gotiColor, col.color))
+				case 5: g.level.AddEntity(tl.NewText(x,y, g.gotya.White.Unta2, gotiColor, col.color))
+				case 6: g.level.AddEntity(tl.NewText(x,y, g.gotya.White.Ghoda2, gotiColor, col.color))
+				case 7: g.level.AddEntity(tl.NewText(x,y, g.gotya.White.Hatti2, gotiColor, col.color))
+				}
+
+			case 7:
+				switch colIndex {
+				case 0: g.level.AddEntity(tl.NewText(x,y, g.gotya.Black.Hatti1, gotiColor, col.color))
+				case 1: g.level.AddEntity(tl.NewText(x,y, g.gotya.Black.Ghoda1, gotiColor, col.color))
+				case 2: g.level.AddEntity(tl.NewText(x,y, g.gotya.Black.Unta1, gotiColor, col.color))
+				case 3: g.level.AddEntity(tl.NewText(x,y, g.gotya.Black.Vajeer, gotiColor, col.color))
+				case 4: g.level.AddEntity(tl.NewText(x,y, g.gotya.Black.Raja, gotiColor, col.color))
+				case 5: g.level.AddEntity(tl.NewText(x,y, g.gotya.Black.Unta2, gotiColor, col.color))
+				case 6: g.level.AddEntity(tl.NewText(x,y, g.gotya.Black.Ghoda2, gotiColor, col.color))
+				case 7: g.level.AddEntity(tl.NewText(x,y, g.gotya.Black.Hatti2, gotiColor, col.color))
+				}
+
+			} 
+		}
 	}
 	g.game.Screen().SetLevel(g.level)
 	g.game.Start()
@@ -59,6 +111,13 @@ func (g Game) paintBoard() {
 func main() {
 	fmt.Println("Starting the game!!")
 	drawnBorad:= constructBoard()
+	file, _ := os.ReadFile("./gotya.json")
+	var gotya Gotya
+	err:= json.Unmarshal(file, &gotya)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(gotya)
 	g := Game{
 		drawnBoard: &drawnBorad,
 		game: tl.NewGame(),
@@ -66,6 +125,7 @@ func main() {
 			Bg: tl.ColorBlue,
 			Ch: '/',
 		}),
+		gotya: gotya,
 	}	
 	g.paintBoard()
 }
